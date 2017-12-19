083feeab39330d63ded52f1275847a35f9591779
James Ko <jamesqko@gmail.com>
2017-12-19 16:06:02 -0500

    More work

--- forking_server.c
+++ forking_server.c
@@ -24,9 +24,29 @@ int main() {
 
 void subserver(int from_client) {
   int to_client = server_connect(from_client);
+
+  char buffer[256];
   while (1) {
+    read(from_client, buffer, sizeof(buffer));
+    process(buffer);
+    write(to_client, buffer, sizeof(buffer));
   }
 }
 
+char rot13(char c) {
+  if (('A' <= c && c <= 'M') || ('a' <= c && c <= 'n')) {
+    return c + 13;
+  }
+  if (('N' <= c && c <= 'Z') || ('n' <= c && c <= 'z')) {
+    return c - 13;
+  }
+
+  return c;
+}
+
 void process(char * s) {
+  while (*s != '\0') {
+    *s = rot13(*s);
+    s++;
+  }
 }
